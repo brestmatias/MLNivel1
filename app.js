ce064f00cfebe0795e9312c96362e4979245dd7b
brestmatias <brestmatias@gmail.com>
2019-05-26 15:01:44 -0300

    Comentarios en app.js

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ const dnaM4 = ["ACTACT","TATGCC","GGAACA","CTTACC","GGGGTT","ACTGAG"];
 const dnaH5 = ["TTGCGA","CTGTGC","TTATGT","AGAAGG","TCCCAA","TCACTG"];
 const dnaH6 = ["ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG"];
 
+//Test DNA Mutant 1
 console.log(mutantChecker.isMutant(dnaM1));
+//Test DNA Mutant 2
 console.log(mutantChecker.isMutant(dnaM2));
+//Test DNA Mutant 3
 console.log(mutantChecker.isMutant(dnaM3));
+//Test DNA Mutant 4
 console.log(mutantChecker.isMutant(dnaM4));
+//Test DNA Human 1
 console.log(mutantChecker.isMutant(dnaH5));
+//Test DNA Human 2
 console.log(mutantChecker.isMutant(dnaH6));
